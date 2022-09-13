480b0428625d1584131edf4eeea4526b4f8ac30f
usmankhanakbar <35627315+usmankhanakbar@users.noreply.github.com>
2022-09-13 22:33:48 +0500

    Kaggle Notebook | TransferLearning | Version 1

---
+++ TransferLearning.py
@@ -0,0 +1,23 @@
+from tensorflow.keras.applications.efficientnet import EfficientNetB7
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Flatten,Dense,Conv2DTranspose,Conv2D
+
+class Transferlearning:
+    def __init__(self):
+        self.shape = Input(shape=(27, 27, 65))  
+        self.model = EfficientNetB7(include_top=False,weights = None,input_tensor=self.shape)
+  
+
+    def customized(self):
+        model = self.model
+    
+        output = model.layers[-13].output
+        output2 = Conv2DTranspose(3,(2,2),strides=(1,1),padding='valid')(output)
+        output2 = Conv2DTranspose(9,(4,4),strides=(1,1),padding='valid')(output2)
+        output2 = Conv2DTranspose(18,(8,8),strides=(1,1),padding='valid')(output2)
+        output2 = Conv2DTranspose(27,(16,16),strides=(1,1),padding = 'valid',activation='tanh')(output2)
+        
+       
+        return model,output2
+        
